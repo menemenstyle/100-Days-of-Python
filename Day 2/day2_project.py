5fd0a7bdbed6f17cfbcb0c6d056ca45585505f39
menemenstyle <117665606+menemenstyle@users.noreply.github.com>
2024-01-25 13:29:43 +0300

    Update day2_project.py

--- Day 2/day2_project.py
+++ Day 2/day2_project.py
@@ -4,6 +4,7 @@ total_bill = float(input("Welcome to the Tip Calculator.\nWhat was the total bil
 tip_percent = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 
-bill_per_person = round((total_bill + total_bill*(tip_percent/100))/people, 2)
+bill_per_person = (total_bill + total_bill*(tip_percent/100))/people
+final_bill = "{:.2f}".format(bill_per_person)
 
-print(f"Each person should pay: ${bill_per_person}")
+print(f"Each person should pay: ${final_bill}")
