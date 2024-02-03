4df352ccdd7ee360397f3766dad2bc9146f32b02
menemenstyle <117665606+menemenstyle@users.noreply.github.com>
2024-02-04 00:27:18 +0300

    Update day8_project.py
    -improved user experience
    -fixed some bugs
    -not finished

--- Day-8/day8_project.py
+++ Day-8/day8_project.py
@@ -4,17 +4,22 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt (encode_text, shift_amount):
-    cypher_text = ""
-    
-    for letters in encode_text:
+def ceaser_cypher(input_message, shift_amount, map):
+    message = ""
+    if map == "decode":
+        shift_amount *= -1
+    for letters in input_message:
         index = alphabet.index(letters)
         position = index + shift_amount
         if position > 26:
-            position -= 26
-        cypher_letters = alphabet[position]
-        cypher_text += cypher_letters
-        
-    print(f"The encoded text is {cypher_text}")
+            i = position % 26
+            position = i
+        elif position < -26:
+            j = position % -26
+            position = j
+        message += alphabet[position]
+    print(f"The {map}d text is {message}")
 
-encrypt(encode_text=text, shift_amount=shift)
+ceaser_cypher(input_message=text, shift_amount=shift, map=direction)
+
+# question = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
