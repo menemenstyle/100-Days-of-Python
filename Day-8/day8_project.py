4416a3d5306b6eafa457a233813edd7d26c26729
menemenstyle <117665606+menemenstyle@users.noreply.github.com>
2024-02-06 17:58:34 +0300

    Update day8_project.py
    -improved user experience
    -fixed bugs

--- Day-8/day8_project.py
+++ Day-8/day8_project.py
@@ -1,25 +1,33 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def ceaser_cypher(input_message, shift_amount, map):
     message = ""
     if map == "decode":
         shift_amount *= -1
     for letters in input_message:
-        index = alphabet.index(letters)
-        position = index + shift_amount
-        if position > 26:
-            i = position % 26
-            position = i
-        elif position < -26:
-            j = position % -26
-            position = j
-        message += alphabet[position]
+        if letters in alphabet:
+            index = alphabet.index(letters)
+            position = index + shift_amount
+            if position > 26:
+                i = position % 26
+                position = i
+            elif position < -26:
+                j = position % -26
+                position = j
+            message += alphabet[position]
+        else:
+            message += letters
     print(f"The {map}d text is {message}")
 
-ceaser_cypher(input_message=text, shift_amount=shift, map=direction)
+loop_input = True
 
-# question = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+while loop_input is True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ceaser_cypher(input_message=text, shift_amount=shift, map=direction)
+    question = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if question == "no":
+        loop_input = False
+        print("Goodbye!")
